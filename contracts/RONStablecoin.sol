8c9c7c082dba400f16b30ee9758062339fe5ce6d
TTC <theomaniac917@gmail.com>
2025-08-01 13:17:48 +0300

    Updated PIF and RON contract declaration

--- contracts/RONStablecoin.sol
+++ contracts/RONStablecoin.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PIFRewards is ERC20, Ownable {
-
-    constructor() ERC20("Pay it Forward Rewards", "PIF") Ownable(msg.sender) {}
+contract RONStablecoin is ERC20, Ownable {
+    
+    constructor() ERC20("RON Stablecoin", "vRON") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         // verify parameters
@@ -16,5 +16,6 @@ contract PIFRewards is ERC20, Ownable {
         // actual mint
         _mint(to, amount);
     }
+    
 }
 

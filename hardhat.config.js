7fff441ae47c0937757af9d91765e715c6b14b07
TTC <theomaniac917@gmail.com>
2025-08-01 12:55:54 +0300

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
   networks: {
     hardhat: {
       // Configuration for the local Hardhat Network
